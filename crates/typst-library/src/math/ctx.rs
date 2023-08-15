21e1970f4b5f9df63a97546c893e9b262eddba1b
Matt Fellenz <matt@felle.nz>
2023-08-14 22:34:19 -0700

    Partially revert 7a1cd362

--- crates/typst-library/src/math/ctx.rs
+++ crates/typst-library/src/math/ctx.rs
@@ -207,27 +207,17 @@ impl<'a, 'b, 'v> MathContext<'a, 'b, 'v> {
                 style = style.with_italic(false);
             }
             let text: EcoString = text.chars().map(|c| style.styled_char(c)).collect();
-            let text = TextElem::packed(text)
-                .styled(TextElem::set_top_edge(TopEdge::Metric(TopEdgeMetric::Bounds)))
-                .styled(TextElem::set_bottom_edge(BottomEdge::Metric(
-                    BottomEdgeMetric::Bounds,
-                )))
-                .spanned(span);
-            let par = ParElem::new(vec![text]);
+            let frame = self.layout_content(
+                &TextElem::packed(text)
+                    .styled(TextElem::set_top_edge(TopEdge::Metric(
+                        TopEdgeMetric::Bounds,
+                    )))
+                    .styled(TextElem::set_bottom_edge(BottomEdge::Metric(
+                        BottomEdgeMetric::Bounds,
+                    )))
+                    .spanned(span),
+            )?;
 
-            // There isn't a natural width for a paragraph in a math environment;
-            // because it will be placed somewhere probably not at the left margin
-            // it will overflow.  So emulate an `hbox` instead and allow the paragraph
-            // to extend as far as needed.
-            let frame = par
-                .layout(
-                    self.vt,
-                    self.outer.chain(&self.local),
-                    false,
-                    Size::splat(Abs::inf()),
-                    false,
-                )?
-                .into_frame();
             FrameFragment::new(self, frame)
                 .with_class(MathClass::Alphabetic)
                 .with_spaced(spaced)
